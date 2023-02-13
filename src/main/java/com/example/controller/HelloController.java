c281d3a09d8e474beb06f9ac5025191bacfc0e6e
hamin <ggamini7@gmail.com>
2023-02-13 13:36:57 +0900

    add ResponseBody

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,18 +1,22 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
+    @ResponseBody
     public String hello() {
         return "Hello, Spring!";
     }
 
     @GetMapping("/test")
+    @ResponseBody
     public String test(@RequestParam("name") String name) {
         System.out.println(name);
         return "Hello, " + name + "!";
